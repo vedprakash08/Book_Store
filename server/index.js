59aa653537b7c5f1839e6f1dc15a248e6b85b25e
vedprakash08 <ved9552231@gmail.com>
2024-07-11 18:36:20 +0530

    after again deploy

--- server/index.js
+++ server/index.js
@@ -16,6 +16,8 @@ app.use(express.json())
 app.use(cors({
     // origin:['http://localhost:5173'],
     origin:['https://book-store-client-murex.vercel.app/'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }))
 app.use(cookieParser())
